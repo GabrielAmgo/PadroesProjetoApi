f19bec81ee01be29261a81271abc5bf5663595ef
Gabriel Amigo <gabrielp.amigo@gmail.com>
2023-04-22 22:38:00 -0300

    initial commit

---
+++ src/main/java/com/digitalinonovationone/PadroesProjetoAPI/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package com.digitalinonovationone.PadroesProjetoAPI.service;
+
+import com.digitalinonovationone.PadroesProjetoAPI.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Client HTTP, criado via <b>OpenFeign</b>, para o consumo da API do
+ * <b>ViaCEP</b>.
+ */
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
---
+++ src/test/java/com/digitalinonovationone/PadroesProjetoAPI/PadroesProjetoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinonovationone.PadroesProjetoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
